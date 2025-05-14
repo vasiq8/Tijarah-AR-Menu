c5f00ccdc8e8b1693d35bd8e8331a1507f4baf9e
vasiq <133151570+vasiq8@users.noreply.github.com>
2025-05-14 13:29:48 +0530

    changed App.css to app.css

--- tiso-homepage/src/app.jsx
+++ tiso-homepage/src/app.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import './App.css';
+import './app.css';
 
 // Navbar images (served from public/assets)
 import strikeImg from './assets/strike.jpeg';
